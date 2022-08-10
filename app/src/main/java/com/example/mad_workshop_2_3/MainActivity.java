feda6d72fc8846d7a9ec7256481576de46ca11bc
20064772 <20064772@student.curtin.edu.au>
2022-08-10 15:52:29 +0800

    see comment in NewActivity

--- app/src/main/java/com/example/mad_workshop_2_3/MainActivity.java
+++ app/src/main/java/com/example/mad_workshop_2_3/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mad_workshop_2_3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +18,22 @@ public class MainActivity extends AppCompatActivity {
         yesButton = (Button) findViewById(R.id.yesButton);
         noButton = (Button) findViewById(R.id.noButton);
 
+        yesButton.setOnClickListener(new View.OnClickListener()
+        {
+
+        // from here down need to be changed. Look at lectures and go from there.
+            @Override
+            public void onClick(View v)
+            {
+                openNewPage();
+            }
+        });
+    }
+
+
+    public void openNewPage()
+    {
+        Intent intent = new Intent(this, NewActivity.class);
+        startActivity(intent);
     }
 }
