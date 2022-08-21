5c25f97279d5d496e1eed70ba4190a6aa3c70abe
ryanmckenney <20064772@student.curtin.edu.au>
2022-08-21 10:03:52 +0800

    matric and imperial UI set. need to implememnt intent for "next" button and calculations/display BMI screen.
    need to look tablet layout. consider new XML for tablet layout.

--- app/src/main/java/com/example/mad_workshop_2_3/Imperial_Activity.java
+++ app/src/main/java/com/example/mad_workshop_2_3/Imperial_Activity.java
@@ -2,12 +2,62 @@ package com.example.mad_workshop_2_3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class Imperial_Activity extends AppCompatActivity {
+    private SeekBar seekHeight;
+    private SeekBar seekWeight;
+    private TextView displayHeight;
+    private TextView displayWeight;
+    private Button Next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imperial);
+
+        SeekBar seekHeight = (SeekBar) findViewById(R.id.seekHeight);
+        SeekBar seekWeight = (SeekBar) findViewById(R.id.seekWeight);
+        TextView displayHeight = (TextView) findViewById(R.id.displayHeight);
+        TextView displayWeight = (TextView) findViewById(R.id.displayWeight);
+        Button Next = (Button) findViewById(R.id.next);
+
+        seekHeight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+               displayHeight.setText("" + i +"in");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+
+            }
+        });
+
+        seekWeight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                displayWeight.setText("" + i + "lb");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
